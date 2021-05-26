95452345e10c336f47db8662beceee81024e45e6
ilyasemikin <iasemikin@gmail.com>
2021-05-27 00:21:15 +0300

    Measurements list added
    hide and show buttons now work
    measurements adds to list

--- software/manual.js
+++ software/manual.js
@@ -1,6 +1,6 @@
 let graphManualChart;
 let measurements = {};
-let i = 0;
+let curMeasureIndex = 0;
 
 function initManualGraph() {
     let options = {
@@ -56,32 +56,87 @@ function initManualGraph() {
     );
 }
 
-function showMeasurement(name, points) {
-    points.forEach(p => console.log(`${p.x}, ${p.y}`));
-    
+function showMeasurement(name, index) {
+    if (measurements[index] == undefined) {
+        return;
+    }
+
+    if (!graphManualChart.data.datasets.every(d => d.mIndex != index)) {
+        return;
+    }
+
+    let points = measurements[index].measure;
+
     let dataset = {
         label: name,
         data: points,
         borderColor: "red",
-        fill: false
+        fill: false,
+        mIndex: index
     }
     graphManualChart.data.datasets.push(dataset);
     graphManualChart.update();
+}
+
+function hideMeasurement(index) {
+    if (index === undefined) {
+        return;
+    }
+
+    let dIdx = graphManualChart.data.datasets.findIndex(x => x.mIndex === index);
+    if (dIdx != -1) {
+        graphManualChart.data.datasets.splice(dIdx, 1);
+        graphManualChart.update();
+    }
+}
+
+function tryShowMeasurement() {
+    let select = document.getElementById("measurements");
+    let index = select.value;
+    showMeasurement(index, index);
+}
+
+function tryHideMeasurement() {
+    let select = document.getElementById("measurements");
+    let index = select.value;
+    hideMeasurement(index);
+}
+
+function saveMeasurement(index, measure) {
+    measurements[index] = {
+        measure: measure,
+        date: Date.now()
+    };
+}
+
+function addMeasureToList(index) {
+    let select = document.getElementById("measurements");
+    let opt = document.createElement("option");
+    opt.value = index;
+    opt.innerHTML = `Измерение ${index}`;
+
+    select.appendChild(opt);
+}
+
+function getRandomData(amount = 3) {
+    return [...Array(amount).keys()]
+        .map(i => ({
+            ampere: Math.random() * 3,
+            volt: i
+        }));
 }
 
 async function measure() {
     let input = document.getElementById("inputChannel");
     let raise = document.getElementById("raiseChannel");
 
-    let data = await fetch(`./metering?in=${input.value}&raise=${raise.value}&from=-3&to=3&step=0.1`)
+    let data = await fetch(`/metering?in=${input.value}&raise=${raise.value}&from=-3&to=3&step=0.1`)
         .then(body => body.json())
         .catch(e => console.log(`Error: ${e}`));
 
-    measurements[`${i}`] = data;
-
-    i++;//номер измерения
-
-    showMeasurement(`Измерение №${i}`, data.map(p => ({ x: p.volt, y: p.ampere })));
+    curMeasureIndex++;
+    saveMeasurement(curMeasureIndex, data.map(p => ({ x: p.volt, y: p.ampere })));
+    addMeasureToList(curMeasureIndex);
 }
 
 initManualGraph();
